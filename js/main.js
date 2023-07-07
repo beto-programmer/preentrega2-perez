03bb43be3b74d5a71691282c92a2612bb9f4749c
Manuel Perez <joicio45@outlook.com>
2023-07-07 19:00:44 -0300

    se comenzo el proyecto con git

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function solicitarCuenta() {
+    alert("Bienvenido a consencionaria Car Luxury! Sigue los pasos para crear tu cuenta")
+    let correo = String(prompt("Ingrese su correo electronico para poder empezar a crear su cuenta"))
+    let contraseña = String(prompt("Ingrese su contraseña"))
+    alert("Listo! Su cuenta ya ha sido creada")
+}
