1be04f377f8c4655c012e85ea80ef378ebd48663
Manuel <joicio45@outlook.com>
2023-07-09 20:27:09 -0300

    Se agrego una lista de motos al codigo de JavaScript

--- js/main.js
+++ js/main.js
@@ -5,13 +5,6 @@ function solicitarCuenta() {
     alert("Listo! Su cuenta ya ha sido creada")
 }
 
-function solicitarVehiculo() {
-    let vehiculo = String(prompt("¿Que tipo de vehiculo prefiere? 1. Autos 2. Motos"))
-    if ("1") {
-        alert ("Genial! Ahora te pasaremos una lista de los autos que tenemos")
-    }
-}
-
 const auto1 =  {
     marca: "Chevrolet",
     modelo: "Corsa",
@@ -71,6 +64,46 @@ const auto6 =  {
     transmisión: "Manual",
     color: "Azul",
     motor: "1.6",
+}
+const listaAutos = [auto1, auto2, auto3, auto4, auto5, auto6]
+const moto1 = {
+    marca: "Yamaha",
+    modelo: "XTZ 150cc",
+    año: 2021,
+    precio: "$1.794.500",
+    color: "Blanco",
+}
+const moto2 = {
+    marca: "Honda",
+    modelo: "Xr Tornado 250cc",
+    año: 2018,
+    precio:"$1.940.000",
+    color: "Negro",
+}
+const moto3 = {
+    marca: "Kawasaki",
+    modelo: "Ninja ZX-6R 600cc",
+    año: 2019,
+    precio: "$11.640.000",
+    color: "Verde",
+}
+const moto4 = {
+    marca: "Yamaha",
+    modelo: "XTZ 250cc",
+    año: 2020,
+    precio: "$2.500.000",
+    color: "Rojo",
+}
+const listaMotos = [moto1, moto2, moto3, moto4]
+function solicitarVehiculo() {
+    let vehiculo = Number(prompt("¿Que tipo de vehiculo prefiere? Escriba 1 si busca auto y 2 si busca moto"))
+    if (vehiculo === 1) {
+        alert ("Genial! Ahora te pasaremos una lista de los autos que tenemos")
+        alert ("1.Corsa 2.")
+    }
+    if (vehiculo === 2){
+        alert ("Genial! Ahora te pasaremos una lista de las motocicletas que tenemos")
+    }
 }
 solicitarCuenta()
 solicitarVehiculo()
