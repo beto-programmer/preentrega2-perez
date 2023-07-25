7f67fe9505c34be38b93bf06d22017e0b6c1b48f
Manuel Perez <joicio45@outlook.com>
2023-07-25 00:24:53 -0300

    Se agrego el precio y se cambio el boton a añadir al carrito

--- js/registro.js
+++ js/registro.js
@@ -3,10 +3,9 @@ signupform.addEventListener('submit', (e)=>{
     e.preventDefault()
     const username = document.querySelector ("#usuario")
     const password = document.querySelector ("#password")
-        
+
     const users = JSON.parse(localStorage.getItem('users')) || []
     users.push({username: username, password: password})
     localStorage.setItem('users', JSON.stringify(users))
     window.location.href = 'pages/inicio.html';
 })
-
