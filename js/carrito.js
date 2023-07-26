8aa755c66349a580ff0f46332af7e6d292284681
Manuel Perez <joicio45@outlook.com>
2023-07-26 12:17:28 -0300

    Se mejoro el carrito de compras

--- js/carrito.js
+++ js/carrito.js
@@ -2,9 +2,70 @@ const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-if (productosEnCarrito) {
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoProductos.classList.remove("disabled")
-} else {
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar")
 
+function cargarProductosCarrito() {
+    if (productosEnCarrito) {
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+    
+        contenedorCarritoProductos.innerHTML = "";
+    
+        productosEnCarrito.forEach(listaAutos => {
+            const div = document.createElement("div");
+            div.classList.add ("carrito-producto");
+            div.innerHTML = `
+            <div class="carrito-producto-titulo">
+            <small>Titulo</small>
+            <h4>${listaAutos.titulo}</h4>
+            </div>
+            <div class="carrito-producto-cantidad">
+            <small>Cantidad</small>
+            <p>${listaAutos.cantidad}</p>
+            </div>
+            <div class="carrito-producto-precio">
+            <small>Precio</small>
+            <p>$${listaAutos.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+            <small>Subtotal</small>
+            <p>$${listaAutos.precio * listaAutos.cantidad}</p>
+            </div>
+            <div>
+            <button class="carrito-producto-eliminar" id="${listaAutos.id}">Eliminar</button>
+            </div>
+            `;
+    
+            contenedorCarritoProductos.append(div);
+    
+        })
+    
+    
+    } else {
+        contenedorCarritoVacio.classList.remove("disabled")
+        contenedorCarritoProductos.classList.add("disabled")
+        contenedorCarritoAcciones.classList.add("disabled")
+    }
+
+    actualizarBotonesEliminar();
+
+}
+
+cargarProductosCarrito();
+actualizarBotonesEliminar();
+
+function actualizarBotonesEliminar (){
+    botonesEliminar = document.querySelectorAll('.carrito-producto-eliminar');
+    botonesEliminar.forEach (boton =>{
+        boton.addEventListener("click", eliminarDelCarrito)
+    })
+}
+
+function eliminarDelCarrito(e){
+    const idBoton = e.currentTarget.id
+    const index = productosEnCarrito.findIndex(listaAutos => listaAutos.id === idBoton)
+    productosEnCarrito.splice(index, 1)
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
 }
--- js/main.js
+++ js/main.js
@@ -3,66 +3,63 @@ const listaAutos = [
         id: "auto1",
         titulo: "Chevrolet Corsa",
         imagen: "../img/Corsa.webp",
-        precio: "$2.600.000",
+        precio: 2600000,
     },
 
 {
     id: "auto2",
     titulo: "Volkswagen Polo",
     imagen: "../img/vw-polo-2021.png",
-    precio: "$5.000.000",
+    precio: 5000000,
 },
 {
     id: "auto3",
     titulo: "Peugeot 308",
     imagen: "../img/blanco.avif",
-    precio: "$10.500.000",
+    precio: 10500000,
 },
 {
     id: "auto4",
     titulo: "Volkswagen Amarok",
     imagen: "../img/vw-amarok-2021.png",
-    precio: "$23.000.000",
+    precio: 23000000,
 },
 {
     id: "auto5",
     titulo: "Audi A3",
     imagen: "../img/Audi-A3-Sedan-Argentina-1.png.webp",
-    precio: "$19.900.000",
+    precio: 19900000,
 },
 {
     id: "auto6",
     titulo: "Ford Focus",
     imagen: "../img/2017-ford-focus-se-sedan-angular-front.avif",
-    precio: "$5.800.000",
-}
-]
-
-const listaMotos = [
+    precio: 5800000,
+},
 {
     id: "moto1",
     titulo: "Yamaha XTZ 150cc",
     imagen: "../img/XTZ150_color_azul.png",
-    precio: "$1.794.500",
+    precio: 1794500,
 },
     
 {
     id: "moto2",
     titulo: "Honda Xr Tornado 250cc",
     imagen: "../img/xr250.png",
-    precio:"$1.940.000",
+    precio: 1940000,
 },
 {
     id: "moto3",
     titulo: "Kawasaki Ninja ZX-6R 600cc",
     imagen: "../img/ninjazx.png",
-    precio: "$11.640.000",
+    precio: 11640000,
 },
 {
     id: "moto4",
     titulo: "Yamaha XTZ 250cc",
     imagen: "../img/xtz250.png",
-    precio: "$2.500.000",
+    precio: 2500000,
 }
 ]
 
@@ -77,30 +74,15 @@ function cargarAutos (){
         div.innerHTML = `
             <img src="${listaAutos.imagen}" alt="${listaAutos.titulo}">
             <h3>${listaAutos.titulo}</h3>
-            <p>${listaAutos.precio}</p>
+            <p>$${listaAutos.precio}</p>
             <button class="tarjeta__btn" id="${listaAutos.id}">Añadir al carro</button>
         `;
         seccionProductos.append(div);
     })
     actualizarBotonesAgregar();
 }
-function cargarMotos(){
-    listaMotos.forEach(listaMotos => {
-        const div = document.createElement("div");
-        div.classList.add("tarjeta")
-        div.innerHTML = `
-            <img src="${listaMotos.imagen}" alt="${listaAutos.titulo}">
-            <h3>${listaMotos.titulo}</h3>
-            <p>${listaMotos.precio}</p>
-            <button class="tarjeta__btn" id="${listaMotos.id}">Añadir al carro</button>
-        `;
-    seccionProductos.append(div);
-})
-actualizarBotonesAgregar();
-}
 
 cargarAutos()
-cargarMotos()
 
 function actualizarBotonesAgregar (){
     botonesAgregar = document.querySelectorAll('.tarjeta__btn');
